0282cd108bca4cd90eae015c085b980369a6f373
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-12-23 00:18:49 +0000

    completed 4th video, learned about setTimer and setInterval

--- #1 code of the play list.js
+++ #1 code of the play list.js
@@ -1,6 +1,33 @@
-console.log("Hello Saif, You are learning Node.js!");
+// var time = 0;
 
-setTimeout(function(){
-    console.log("Three seconds \n have passed!");
+// var timer = setInterval(function () {
+//     time += 2;
+//     console.log(time + ' seconds have passed');
+//     if (time > 5) {
+//         clearInterval(timer);
+//     }
+// }, 2000);  SUPER COOL TIMER THING done in video 4 on nodejs
 
-},3000);
+console.log(__dirname)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Hello Saif, You are learning Node.js!");
+
+// setTimeout(function(){
+//     console.log("Three seconds \n have passed!");
+
+// },3000);
