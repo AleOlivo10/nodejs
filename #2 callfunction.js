b84f02b7db824cb1d3b90869b12efd07c59b8312
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-12-24 18:15:08 +0000

    normal function statement, function expressions and passing a function through another function

--- #2 callfunction.js
+++ #2 callfunction.js
@@ -11,14 +11,16 @@ var sayBye = function() {   //this is an anonymous function. variable is being m
     console.log('bye');
 }
 
-sayBye();       //call on the function expression the same way as above normal function
+// sayBye();       //call on the function expression the same way as above normal function
 //function expressions are common in node.js and I should expect to see more of it
 
-function callFunction(func) {
+
+//we can pass functions from one thing to another
+function callFunction(func) {       //it's taking a function as a parameter
     func();
 }
 
-a;ilsdjf
+callFunction(sayBye)    //the callFuction() is being used to call on the sayBye() and you get the same result as when using just sayBye() by itself
 
 
 
