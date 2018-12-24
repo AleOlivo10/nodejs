9172e9cc0d92cd7e195d6936cc2248f99b28db08
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-12-24 02:59:51 +0000

    function expressions

--- #2 callfunction.js
+++ #2 callfunction.js
@@ -1,9 +1,33 @@
-function callFunction(fun){
- fun();
+//normal function statement:
+function sayHi() {
+    console.log('hi');
 }
 
-var tata = function(){
+sayHi();
+
+
+//function expression:
+var sayBye = function() {   //this is an anonymous function. variable is being made equal to a nameless function
     console.log('bye');
 }
 
-callFunction(tata);
+sayBye();       //call on the function expression the same way as above normal function
+//function expressions are common in node.js and I should expect to see more of it
+
+
+
+
+
+
+
+
+
+// function callFunction(fun){
+//  fun();
+// }
+
+// var tata = function(){
+//     console.log('bye');
+// }
+
+// callFunction(tata);
