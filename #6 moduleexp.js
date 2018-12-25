aaad2d367d90e378f4cd420957ca57be87ddf074
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-12-25 22:31:19 +0000

    started lesson

--- #6 moduleexp.js
+++ #6 moduleexp.js
@@ -1,19 +1,46 @@
 
-// This file is completely a module for getmodule.js file.
 
 
-// a function expression is made here.
-var counter = function(arry){
-    return 'The number of '+arry.length + '\n done!';
-};
-// Adding more modules
-var adder = function(a,b){
-    return 'On adding the two number it gives'+(a+b);
+var counter = function(arr) {
+    return "There are " + arr.length + " elements in this array.";
 }
 
-var pi = 3.1535;
+console.log(counter(["alejandra", "john", "jayne"]));
 
-// module.exports is the important part, it makes the counter available for other modules!
-module.exports.counter = counter ;
-module.exports.adder = adder;
-module.exports.bi = pi;
+//this code us for a function that tells you how many items there are within an array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // This file is completely a module for getmodule.js file.
+
+
+// // a function expression is made here.
+// var counter = function(arry){
+//     return 'The number of '+arry.length + '\n done!';
+// };
+// // Adding more modules
+// var adder = function(a,b){
+//     return 'On adding the two number it gives'+(a+b);
+// }
+
+// var pi = 3.1535;
+
+// // module.exports is the important part, it makes the counter available for other modules!
+// module.exports.counter = counter ;
+// module.exports.adder = adder;
+// module.exports.bi = pi;
