c18ab33d8c53751deec06702b3eeedd6bffd3113
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-12-27 02:42:44 +0000

    lesson on custom event listeners and built-in modules found in node.js

--- #4 event.js
+++ #4 event.js
@@ -1,9 +1,45 @@
+//there are core modules that ppl can request that are a part of node.js, such as the event module
+//whatever is returned from the module.exports pertaining to 'events will now be stored in the variable 'events'
 var events = require('events');
 
-var myEmmitter = new events.EventEmitter();
+var myEmmitter = new events.EventEmitter(); //myEmitter is now equal to this feature that's part of the event module
 
-myEmmitter.on('anEvent',function(msg){
-    console.log(msg);
+myEmmitter.on('someEvent', function(mssg) { //when this event (someEvent) happens, run the following function which has a parameter of mssg
+    console.log(mssg)
 });
 
-myEmmitter.emit('anEvent','The event is absolutely emmited');
+myEmmitter.emit('someEvent', "the event was emitted");
+// ----------------------------------------------
+
+var util = require("util")
+
+var Person = function(name) {
+    this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter); //Person parameter is the thing inheriting something, events.EventEmitter is the thing being inherited
+
+var james = new Person('james');
+var eleanor = new Person('eleanor');
+var rita = new Person('rita');
+var people = [james, eleanor, rita];
+
+people.forEach(function(person) {       //we're able to attach this function to each person because we activated this event listener onto Person on line 20
+  person.on('speak', function(mssg) {
+      console.log(person.name + " said: " + mssg);
+  });  
+});
+
+james.emit('speak', "hey dudes"); //this is how event is emitted and line 28-31 is how we attached it to each name
+eleanor.emit('speak', 'cowabunga');
+rita.emit('speak', 'coding is gnarly');
+
+// var events = require('events');
+
+// var myEmmitter = new events.EventEmitter();
+
+// myEmmitter.on('anEvent',function(msg){
+//     console.log(msg);
+// });
+
+// myEmmitter.emit('anEvent','The event is absolutely emmited');
