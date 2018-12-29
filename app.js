58e718c0600ac1a386903f8ffdd355730fb31f38
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-12-29 20:15:09 +0000

    this is a sandbox

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-var fs = require('fs');
+var counter = function(arr) {
+    return "There are " + arr.length + " elements in this array.";
+}
 
-var file = fs.readFile('input.txt','utf8',function(err,data){
-    fs.writeFile('writeme.txt',data);
-    console.log(data);
-});
+console.log(counter(["alejandra", "john", "jayne"]));
 
-console.log('This is an instruction outside the sync file system.');
+//this code us for a function that tells you how many items there are within an array
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fs = require('fs');
+
+// var file = fs.readFile('input.txt','utf8',function(err,data){
+//     fs.writeFile('writeme.txt',data);
+//     console.log(data);
+// });
+
+// console.log('This is an instruction outside the sync file system.');
