0436d3dc249852841dab2659f74f8d2f70a87173
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-12-31 05:43:41 +0000

    doesnt work for me. Stick with c9 git commands for creating directories. Can deff create files tho

--- #3 dirname.js
+++ #3 dirname.js
@@ -1,4 +1,40 @@
-// node can also determine the your directory by using '__dirname';
+var fs = require('fs');
 
-console.log(__dirname);
-console.log(__filename);
+
+//creating directories:
+fs.mkdir('stuff', function() {
+    fs.readFile('readMe.txt', 'uft8', function(error, data) {
+        fs.writeFile('./stuff/writeMe.txt', data);
+    })
+});
+
+
+
+//the above is an async way to create directory.
+
+//doesnt work for me but that's ok
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // node can also determine the your directory by using '__dirname';
+
+// console.log(__dirname);
+// console.log(__filename);
